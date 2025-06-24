098eca8289da170218f13e54fdeb0116592fe095
nikita-kotliar <knr200710@gmail.com>
2025-06-24 06:27:20 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ const whitelist = [
   'https://recipe-book-ruddy-iota.vercel.app',
   'https://recipe-book-ojfs37rwk-nikita-kotliars-projects.vercel.app',
   'http://localhost:5173',
+  'https://recipe-book-git-main-nikita-kotliars-projects.vercel.app',
 ];
 
 const corsOptions = {
